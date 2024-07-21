48dff03e51a9707e4536e3ecf462990cab6d1753
sha-255 <fawsgre@mail.ru>
2024-07-21 17:20:51 +0600

    get cards by userId
    in API: [base url]/cards/userId/[userId: number]

--- src/cards/cards.service.ts
+++ src/cards/cards.service.ts
@@ -1,35 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 import { Card } from 'src/dto/card.dto';
 
 @Injectable()
 export class CardsService {
-    constructor(private readonly databaseService: DatabaseService) {}
+  constructor(private readonly databaseService: DatabaseService) {}
 
-    async getUserCards(userNumber: number): Promise<Card[]> {
-        return
-        // return (await this.databaseService.user.findMany({ where: { id: userNumber }, include: {cards: true} })) as Card[];
+  async getUserCards(userId: number): Promise<Card[]> {
+    const user = await this.databaseService.user.findFirst({
+      where: {
+        id: Number.parseInt(userId.toString()),
+      },
+    });
+    if (user) {
+      return await this.databaseService.card.findMany({
+        where: {
+          userId: Number.parseInt(userId.toString()),
+        },
+      });
     }
+    throw new NotFoundException();
+  }
 
-    async addCard(card: Card) {
-        return await this.databaseService.card.create({
-            data: {
-                userId: card.userId,
-                pool: card.pool,
-                number: card.number,
-                reloadTime: card.reloadTime,
-                reloadStartTime: card.reloadStartTime,
-                cost: card.cost,
-                imageUri: card.imageUri,
-            }
-        })
-    }
+  async addCard(card: Card) {
+    return await this.databaseService.card.create({
+      data: {
+        userId: card.userId,
+        pool: card.pool,
+        number: card.number,
+        reloadTime: card.reloadTime,
+        reloadStartTime: card.reloadStartTime,
+        cost: card.cost,
+        imageUri: card.imageUri,
+      },
+    });
+  }
 
-    async deleteCard(id: number) {
-        return await this.databaseService.card.delete({
-            where: {
-              id: id,
-            },
-          });
-    }
+  async deleteCard(id: number) {
+    return await this.databaseService.card.delete({
+      where: {
+        id: id,
+      },
+    });
+  }
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,32 +1,40 @@
-import { Controller, Delete, Get, ParseIntPipe, Post, Param, Body, UseGuards, forwardRef, Inject } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  ParseIntPipe,
+  Post,
+  Param,
+  Body,
+  UseGuards,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UserToAdd } from 'src/dto/user.dto';
 import { AuthGuard } from 'src/auth/auth.guard';
-import { AuthService } from 'src/auth/auth.service';
 
 @Controller('users')
 export class UsersController {
-    constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) {}
 
-    @Get()
-    @UseGuards(AuthGuard)
-    async getUsers(): Promise<UserToAdd[]> {
-        return await this.usersService.getUsers();
-    }
+  @Get()
+  //@UseGuards(AuthGuard)
+  async getUsers(): Promise<UserToAdd[]> {
+    return await this.usersService.getUsers();
+  }
 
-    @Post()
-    async addUsers() {
-        return await this.usersService.addUser();
-    }
+  @Post()
+  async addUsers() {
+    return await this.usersService.addUser();
+  }
 
-    @Delete('/:id')
-    async deleteById(@Param('id', ParseIntPipe) id: number) {
-        return await this.usersService.deleteUser(id);
-    }
+  @Delete('/:id')
+  async deleteById(@Param('id', ParseIntPipe) id: number) {
+    return await this.usersService.deleteUser(id);
+  }
 
-    @Get('main')
-    @UseGuards(AuthGuard)
-    async getMain(@Body('id', ParseIntPipe) id: number) {
-        return await this.usersService.getMain(id);
-    }
+  @Get('main')
+  @UseGuards(AuthGuard)
+  async getMain(@Body('id', ParseIntPipe) id: number) {
+    return await this.usersService.getMain(id);
+  }
 }
