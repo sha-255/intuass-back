30712b5697c5d6e86d732da33117a78f59624b83
sha-255 <fawsgre@mail.ru>
2024-07-21 23:44:25 +0600

    user info hotfixe

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,27 +4,41 @@ import { UserToAdd } from 'src/dto/user.dto';
 
 @Injectable()
 export class UsersService {
-    constructor(private readonly databaseService: DatabaseService) {}
+  constructor(private readonly databaseService: DatabaseService) {}
 
-    async addUser() {
-        return await this.databaseService.user.create({
-            data: {}
-        });
-    }
+  async addUser() {
+    return await this.databaseService.user.create({
+      data: {},
+    });
+  }
 
-    async getUsers(): Promise<UserToAdd[]> {
-        return (await this.databaseService.user.findMany({
-            include: {
-                wallet: true
-            }
-        })) as UserToAdd[];
-    }
+  async getUsers(): Promise<UserToAdd[]> {
+    return (await this.databaseService.user.findMany({
+      include: {
+        wallet: true,
+      },
+    })) as UserToAdd[];
+  }
 
-    async deleteUser(id: number) {
-        return await this.databaseService.user.delete({
-            where: {
-                id,
-            }
-        })
-    }
+  async deleteUser(id: number) {
+    return await this.databaseService.user.delete({
+      where: {
+        id,
+      },
+    });
+  }
+
+  async getMain(id: number) {
+    return await this.databaseService.user.findFirst({
+      where: { id },
+      include: {
+        wallet: {
+          include: {
+            stake: true,
+          },
+        },
+        cards: true,
+      },
+    });
+  }
 }
