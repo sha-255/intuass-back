0ac1d789470f3a72b21c00b00c651a01cfb448d5
Pasyda <rep_er11@mail.ru>
2024-09-24 21:19:20 +0600

    +logIn

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,31 +1,35 @@
-import { Body, Controller, Get, Post, UseGuards, Request } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UseGuards,
+  Request,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { UserToAdd } from 'src/dto/user.dto';
 import { AuthGuard } from './auth.guard';
 
 @Controller()
 export class AuthController {
-    constructor(private readonly authService: AuthService) {}
+  constructor(private readonly authService: AuthService) {}
 
-    @Post('registrate')
-    async registrate(@Body() userData: { address: string }): Promise<{accessToken: string}> {
-        return await this.authService.registrate(userData.address);
-    }
+  @Post('signIn')
+  async signIn(
+    @Body() userData: { address: string },
+  ): Promise<{ accessToken: string }> {
+    return await this.authService.logIn(userData.address);
+  }
 
-    @Post('signIn')
-    async signIn(@Body() userData: { address: string }): Promise<{accessToken: string}> {
-        return await this.authService.signIn(userData.address)
-    }
+  @UseGuards(AuthGuard)
+  @Get('wallet')
+  async getWallet(@Request() req) {
+    return req.user.username;
+  }
 
-    @UseGuards(AuthGuard)
-    @Get('wallet')
-    async getWallet(@Request() req) {
-        return req.user.username
-    }
-
-    @UseGuards(AuthGuard)
-    @Get('user')
-    async getUser(@Request() req) {
-        return await this.authService.getUser(req.user.username)
-    }
+  @UseGuards(AuthGuard)
+  @Get('user')
+  async getUser(@Request() req) {
+    return await this.authService.getUser(req.user.username);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -5,62 +5,77 @@ import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-    constructor(private readonly UsersService: UsersService, private readonly DatabaseService: DatabaseService, private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly UsersService: UsersService,
+    private readonly DatabaseService: DatabaseService,
+    private readonly jwtService: JwtService,
+  ) {}
 
-    async registrate(address: string): Promise<{accessToken}> {
-        const user = await this.UsersService.addUser()
-        await this.DatabaseService.wallet.create({
-            data: { userId: user.id, address, inas:0, reward:0 } 
-        })
-        return {
-            accessToken: await this.jwtService.signAsync({
-                sub: user.id,
-                username: address
-            })
-        }
+  async logIn(address: string): Promise<{ accessToken }> {
+    let userData;
+    const check = !!(await this.DatabaseService.wallet.findFirst({
+      where: { address: address },
+    }));
+    console.log(check);
+    if (check) {
+      userData = await this.signIn(address);
+    } else {
+      userData = await this.registrate(address);
     }
+    return {
+      accessToken: await this.jwtService.signAsync({
+        sub: userData.userId,
+        username: userData.address,
+      }),
+    };
+  }
 
-    async signIn(address: string): Promise<{accessToken}> {
-        const user = await this.DatabaseService.wallet.findFirst({
-            where: {address: address}
-        })
-        
-        return {
-            accessToken: await this.jwtService.signAsync({
-                sub: user.userId,
-                username: user.address,
-            })
-        }
-    }
+  async signIn(address: string) {
+    const user = await this.DatabaseService.wallet.findFirst({
+      where: { address: address },
+    });
+    return user;
+  }
 
-    async getWallet(id: number): Promise<{walletId}> {
-        const wallet = await this.DatabaseService.user.findFirst({
-            where: {id: id},
-            include: {wallet: true}
-        })
-        return {
-            walletId: wallet.wallet.address
-        }
-    }
+  async registrate(address: string) {
+    const newUser = await this.UsersService.addUser();
+    await this.DatabaseService.wallet.create({
+      data: { userId: newUser.id, address, inas: 0, reward: 0 },
+    });
+    const user = await this.DatabaseService.wallet.findFirst({
+      where: { address: address },
+    });
+    return user;
+  }
 
-    async getUser(address: string): Promise<{user}> {
-        const wallet = await this.DatabaseService.wallet.findFirst({
-            where: {address: address}
-        })
+  async getWallet(id: number): Promise<{ walletId }> {
+    const wallet = await this.DatabaseService.user.findFirst({
+      where: { id: id },
+      include: { wallet: true },
+    });
+    return {
+      walletId: wallet.wallet.address,
+    };
+  }
 
-        const user = await this.DatabaseService.user.findFirst({
-            where: {id: wallet.userId},
-            include: {
-                wallet: {
-                include: {
-                    stake: true,
-                },
-                },
-                cards: true,
-            },
-        })
-        return {
-            user: user
-        }
-    }
+  async getUser(address: string): Promise<{ user }> {
+    const wallet = await this.DatabaseService.wallet.findFirst({
+      where: { address: address },
+    });
+
+    const user = await this.DatabaseService.user.findFirst({
+      where: { id: wallet.userId },
+      include: {
+        wallet: {
+          include: {
+            stake: true,
+          },
+        },
+        cards: true,
+      },
+    });
+    return {
+      user: user,
+    };
+  }
 }
